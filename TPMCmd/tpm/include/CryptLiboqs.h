02efb8b976dbe55241c976f3c75ba888ab7f9491
Palatrauss00 <davide.palatroni@gmail.com>
2025-03-25 20:59:38 +0100

    Add new sphincs files

---
+++ TPMCmd/tpm/include/CryptLiboqs.h
@@ -0,0 +1,11 @@
+#ifndef CRYPT_LIBOQS_H
+#define CRYPT_LIBOQS_H
+
+#include "Tpm.h"
+
+// Dichiarazione della funzione
+TPM_RC CryptSphincsGenerateKeyPair(TPMT_PUBLIC *publicArea, TPMT_SENSITIVE *sensitive, RAND_STATE *rand);
+
+TPM_RC CryptSphincsSign(TPMT_SIGNATURE* sigOut, OBJECT* key, TPM2B_DIGEST* digest, RAND_STATE* rand);
+
+#endif // CRYPT_LIBOQS_H
